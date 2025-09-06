fdb611bc73cca8406d211eb0b336b35bd20c3829
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-09-06 15:07:26 +0300

    Character move

---
+++ Player/Scripts/state.gd
@@ -0,0 +1,15 @@
+class_name State  extends Node
+
+static var player : Player
+
+func  _ready():
+	pass
+	
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func process(_delta : float) -> State:
+	return null
