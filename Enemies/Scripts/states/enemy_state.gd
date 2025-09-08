c3b9a8221f65751f4074342d592bfaec5b85823b
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-09-08 13:40:54 +0300

    Make a Slime Enemy

---
+++ Enemies/Scripts/states/enemy_state.gd
@@ -0,0 +1,19 @@
+class_name EnemyState extends Node
+
+var enemy : Enemy
+var state_machine : EnemyStateMachine 
+
+func  init():
+	pass
+	
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func process(_delta : float) -> EnemyState:
+	return null
+	
+func physics(_delta : float) -> EnemyState:
+	return null
