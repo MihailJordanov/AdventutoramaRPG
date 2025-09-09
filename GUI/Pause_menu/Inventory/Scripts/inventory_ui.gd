540af337ef050f9002a55537371f83e8e3411684
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-09-10 01:01:48 +0300

    Resource Based Inventory System

---
+++ GUI/Pause_menu/Inventory/Scripts/inventory_ui.gd
@@ -0,0 +1,26 @@
+class_name InventoryUI extends Control
+
+const INVENTORY_SLOT = preload("res://GUI/Pause_menu/Inventory/inventory_slot.tscn")
+
+@export var data : InventoryData
+
+func _ready() -> void:
+	PauseMenu.shown.connect( update_inventory )
+	PauseMenu.hidden.connect( clear_invenory )
+	clear_invenory()
+	pass
+
+
+func clear_invenory() -> void:
+	for c in get_children():
+		c.queue_free()
+	
+func update_inventory() -> void:
+	for s in data.slots:
+		var new_slot = INVENTORY_SLOT.instantiate()
+		add_child( new_slot )
+		new_slot.slot_data = s
+	
+	get_child( 0 ).grab_focus()
+	
+	
