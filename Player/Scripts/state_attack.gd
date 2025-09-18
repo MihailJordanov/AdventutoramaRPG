08e0418123dcf561d891357be22250950a2713fd
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2025-09-18 15:56:57 +0300

    Enemy Item Drops

--- Player/Scripts/state_attack.gd
+++ Player/Scripts/state_attack.gd
@@ -25,13 +25,14 @@ func Enter():
 	attacking = true
 	
 	await get_tree().create_timer( 0.075 ).timeout
-	hurt_box.monitoring = true
+	if attacking:
+		hurt_box.monitoring = true
 	pass
 	
 func Exit():
 	animation_player.animation_finished.disconnect( EndAttack )
 	attacking = false
-	hurt_box.monitoring = false	
+	hurt_box.monitoring = false
 	pass
 	
 func Process(_delta : float) -> State:
